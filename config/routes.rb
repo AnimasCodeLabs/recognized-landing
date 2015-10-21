4872548d66dc2e672f4157ec8bf99d2b65ffdd2f
Aaron Renner <aaron@animascodelabs.com>
2015-10-21 11:20:32 -0600

    Legal pages and adjustments to header

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get "terms_of_service" => "legal#terms_of_service"
+  get "privacy" => "legal#privacy"
+
   root "landing#index"
 end
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -6,4 +6,16 @@ feature 'Static pages' do
 
     expect(page).to have_content("Recognized.io")
   end
+
+  scenario 'User views the terms of service page' do
+    visit "/terms_of_service"
+
+    expect(page).to have_content("Terms of Service")
+  end
+
+  scenario 'User views the privacy policy page' do
+    visit "/privacy"
+
+    expect(page).to have_content("Privacy Policy")
+  end
 end
